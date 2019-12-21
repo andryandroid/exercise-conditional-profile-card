1a8c17e57cdefcbd59dedb6735a36d4361430e21
andryandroid <andjos27@gmail.com>
2019-12-21 17:06:07 +0000

    [ejercicio completado]

--- public/bundle.js
+++ public/bundle.js
@@ -701,30 +701,68 @@ __webpack_require__.r(__webpack_exports__);
  *  This function is called every time the user changes types or changes any input
  */
 function render(variables = {}) {
-  // here we ask the logical questions to make decitions on how to build the heml
+  // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
+
+  let name = "Name";
+  if (variables.name != null) name = variables.name;
+
+  let lastname = "lastname";
+  if (variables.lastname != null) lastname = variables.lastname;
+
+  let city = "city";
+  if (variables.city != null) city = variables.city;
+
+  let country = "country";
+  if (variables.country != null) country = variables.country;
+
+  let rol = "";
+  if (variables.role != null) rol = variables.role;
+
+  let socialMediaPosition = "left";
+  if (variables.socialMediaPosition != null) socialMediaPosition = variables.socialMediaPosition;
+
+  let role = "";
+  if (variables.role != null) role = variables.role;
+
+  let socialTwitter = "";
+  if (variables.twitter != null) {
+    socialTwitter = `<li><a href='https://twitter.com/${variables.twitter}'><i class="fa fa-twitter"></i></a></li>`;
+  }
+  let socialGithub = "";
+  if (variables.github != null) {
+    socialGithub = `<li><a href="https://github.com/${variables.github}"><i class="fa fa-github"></i></a></li>`;
+  }
+  let socialLinkedin = "";
+  if (variables.linkedin != null) {
+    socialLinkedin = `<li><a href="https://linkedin.com/${variables.linkedin}"><i class="fa fa-linkedin"></i></a></li>`;
+  }
+  let socialInstagram = "";
+  if (variables.instagram != null) {
+    socialInstagram = `<li><a href="https://instagram.com/${variables.instagram}"><i class="fa fa-instagram"></i></a></li>`;
+  }
 
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/alesanchezr"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://github.com/alesanchezr"><i class="fa fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/alesanchezr"><i class="fa fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/alesanchezr"><i class="fa fa-instagram"></i></a></li>
+          <h1>${name} ${lastname}</h1>
+          <h2>${rol}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class="${socialMediaPosition}">
+            ${socialTwitter}
+            ${socialGithub}
+            ${socialLinkedin}
+            ${socialInstagram}
           </ul>
         </div>
     `;
 }
 
 /**
- * Ignore this lines below, here is where we do the logic for the dropdowns
+ * Don't change any of the lines below, here is where we do the logic for the dropdowns
  */
 window.onload = function () {
   window.variables = {
@@ -735,10 +773,10 @@ window.onload = function () {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "left",
+    socialMediaPosition: "position-left",
     // social media usernames
     twitter: null,
-    github: "alesanchezr",
+    github: null,
     linkedin: null,
     instagram: null,
     name: null,
